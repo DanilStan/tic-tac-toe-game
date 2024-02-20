3aafe0608a0523cffcd60403225abdc8e2f4aff0
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-02-20 16:44:13 +0200

    Test

--- tic-tac-toe-game/src/app/hello-section/hello-section.component.ts
+++ tic-tac-toe-game/src/app/hello-section/hello-section.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hello-section',
   templateUrl: './hello-section.component.html',
-  styleUrl: './hello-section.component.scss'
+  styleUrl: './hello-section.component.scss',
 })
 export class HelloSectionComponent {
-
+  // title: string;
 }
